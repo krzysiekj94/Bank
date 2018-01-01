121a0da043a99928c35b58ddc2dc5577699edb87
Krzysiek <k.jerzynski1@gmail.com>
2018-01-01 18:30:39 +0100

    + add new unit tests + delete old RaportTests.cs

--- BankTestyJednostkowe/SumaSaldTests.cs
+++ BankTestyJednostkowe/SumaSaldTests.cs
@@ -1,14 +1,96 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bank.RaportVisitor;
+using Bank;
+using Bank.Mediator;
 
 namespace BankTestyJednostkowe
 {
     [TestClass]
     public class SumaSaldTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        SumaSald raportSumaSald;
+        RachunekDebetowy rachunekDebetowy;
+        Lokata lokata;
+        Kredyt kredyt;
+
+        long kredytSaldo;
+        long lokataSaldo;
+        long rachunekDebetowySaldo;
+
+        void WczytajDaneTestowe()
         {
+            kredytSaldo = 250;
+            lokataSaldo = 12000;
+            rachunekDebetowySaldo = 1300;
+
+            raportSumaSald = new SumaSald();
+            kredyt = new KredytObj();
+            lokata = new Lokata();
+            kredyt.Saldo = kredytSaldo;
+            lokata.Saldo = lokataSaldo;
+
+            rachunekDebetowy = new RachunekDebetowy(new Rachunek());
+            rachunekDebetowy.Saldo = rachunekDebetowySaldo;
+
+        }
+
+        [TestMethod]
+        public void SumaSaldRownaSumieDanychTestowych()
+        {
+            WczytajDaneTestowe();
+            long SumaSaldStale = kredytSaldo + lokataSaldo + rachunekDebetowySaldo;
+            long SumaSaldVisitor = 0;
+
+            raportSumaSald.Visit(kredyt);
+            raportSumaSald.Visit(lokata);
+            raportSumaSald.Visit(rachunekDebetowy);
+            SumaSaldVisitor = raportSumaSald.Calculate( new BankType( "test", new KIR() ));
+
+            Assert.IsTrue(SumaSaldStale == SumaSaldVisitor );
+        }
+
+        [TestMethod]
+        public void SumaSaldProduktowBankowychWiekszaOdZera()
+        {
+            WczytajDaneTestowe();
+            raportSumaSald.Visit(kredyt);
+            raportSumaSald.Visit(lokata);
+            raportSumaSald.Visit(rachunekDebetowy);
+            long SumaSaldVisitor = raportSumaSald.Calculate(new BankType("test", new KIR()));
+
+            Assert.IsTrue(SumaSaldVisitor > 0);
+        }
+
+        [TestMethod]
+        public void SumaSaldRownaSalduKredytuPoJegoOdwiedzeniu()
+        {
+            WczytajDaneTestowe();
+            raportSumaSald.Visit(kredyt);
+            long SumaSaldVisitor = raportSumaSald.Calculate(new BankType("test", new KIR()));
+
+            Assert.IsTrue(SumaSaldVisitor == kredytSaldo );
+        }
+
+        [TestMethod]
+        public void SumaSaldRownaSalduLokatyPoJejOdwiedzeniu()
+        {
+            WczytajDaneTestowe();
+            raportSumaSald.Visit(lokata);
+            long SumaSaldVisitor = raportSumaSald.Calculate(new BankType("test", new KIR()));
+
+            Assert.IsTrue(SumaSaldVisitor == lokataSaldo);
+        }
+
+
+        [TestMethod]
+        public void SumaSaldRownaSalduRachunkuDebetowegoPoJegoOdwiedzeniu()
+        {
+            WczytajDaneTestowe();
+            raportSumaSald.Visit(rachunekDebetowy);
+            long SumaSaldVisitor = raportSumaSald.Calculate(new BankType("test", new KIR()));
+
+            Assert.IsTrue(SumaSaldVisitor == rachunekDebetowySaldo);
         }
     }
 }
--- BankTestyJednostkowe/SumaZadluzeniaTests.cs
+++ BankTestyJednostkowe/SumaZadluzeniaTests.cs
@@ -1,14 +1,98 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bank.RaportVisitor;
+using Bank;
+using Bank.Mediator;
 
 namespace BankTestyJednostkowe
 {
     [TestClass]
     public class SumaZadluzeniaTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        SumaZadluzenia raportSumaZadluzenia;
+        RachunekDebetowy rachunekDebetowy;
+        Lokata lokata;
+        Kredyt kredyt;
+
+        long kredytSaldo;
+        long lokataSaldo;
+        long rachunekDebetowySaldo;
+
+        void WczytajDaneTestowe()
         {
+            kredytSaldo = -5000;
+            lokataSaldo = 12000;
+            rachunekDebetowySaldo = -2000;
+
+            raportSumaZadluzenia = new SumaZadluzenia();
+            kredyt = new KredytObj();
+            lokata = new Lokata();
+
+            kredyt.Saldo = kredytSaldo;
+            lokata.Saldo = lokataSaldo;
+
+            rachunekDebetowy = new RachunekDebetowy(new Rachunek());
+            rachunekDebetowy.Saldo = rachunekDebetowySaldo;
+
+        }
+
+        [TestMethod]
+        public void SumaZadluzeniaRownaSumieDanychTestowych()
+        {
+            WczytajDaneTestowe();
+
+            long SumaZadluzeniaZmienne = kredytSaldo + rachunekDebetowySaldo;
+            long SumaZadluzeniaVisitor = 0;
+            raportSumaZadluzenia.Visit(kredyt);
+            raportSumaZadluzenia.Visit(lokata);
+            raportSumaZadluzenia.Visit(rachunekDebetowy);
+
+            SumaZadluzeniaVisitor = raportSumaZadluzenia.Calculate(new BankType("test", new KIR()));
+
+            Assert.IsTrue( SumaZadluzeniaZmienne == SumaZadluzeniaVisitor );
+        }
+
+        [TestMethod]
+        public void SumaZadluzeniaProduktowBankowychKwotaMniejszaOdZera()
+        {
+            WczytajDaneTestowe();
+            raportSumaZadluzenia.Visit(kredyt);
+            raportSumaZadluzenia.Visit(lokata);
+            raportSumaZadluzenia.Visit(rachunekDebetowy);
+            long SumaZadluzeniaVisitor = raportSumaZadluzenia.Calculate(new BankType("test", new KIR()));
+
+            Assert.IsTrue(SumaZadluzeniaVisitor < 0);
+        }
+
+        [TestMethod]
+        public void SumaZadluzeniaRownaSalduKredytuPoJegoOdwiedzeniu()
+        {
+            WczytajDaneTestowe();
+            raportSumaZadluzenia.Visit(kredyt);
+            long SumaZadluzeniaVisitor = raportSumaZadluzenia.Calculate(new BankType("test", new KIR()));
+
+            Assert.IsTrue(SumaZadluzeniaVisitor == kredytSaldo);
+        }
+
+        [TestMethod]
+        public void SumaZadluzeniaNieDotyczyLokatyPoJejOdwiedzeniuRowna0()
+        {
+            WczytajDaneTestowe();
+            raportSumaZadluzenia.Visit(lokata);
+            long SumaZadluzeniaVisitor = raportSumaZadluzenia.Calculate(new BankType("test", new KIR()));
+
+            Assert.IsTrue(SumaZadluzeniaVisitor == 0);
+        }
+
+
+        [TestMethod]
+        public void SumaZadluzeniaRownaSalduRachunkuDebetowegoPoJegoOdwiedzeniu()
+        {
+            WczytajDaneTestowe();
+            raportSumaZadluzenia.Visit(rachunekDebetowy);
+            long SumaZadluzeniaVisitor = raportSumaZadluzenia.Calculate(new BankType("test", new KIR()));
+
+            Assert.IsTrue(SumaZadluzeniaVisitor == rachunekDebetowySaldo);
         }
     }
 }
