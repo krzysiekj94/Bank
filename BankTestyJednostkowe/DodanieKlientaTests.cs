8661ad1bd9ad1fdbf3629f3b588c9fbc071c6c65
krzysiekj94 <k.jerzynski1@gmail.com>
2018-01-01 14:56:23 +0100

    + new tests files

---
+++ BankTestyJednostkowe/DodanieKlientaTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankTestyJednostkowe
+{
+    [TestClass]
+    public class DodanieKlientaTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
---
+++ BankTestyJednostkowe/OtworzProduktBankowyTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankTestyJednostkowe
+{
+    [TestClass]
+    public class OtworzProduktBankowyTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
